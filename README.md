81567aed78ff4d885642b5bf168789f2105832f5
DeleteriousHoming <rhagionid_parsettensite@aleeas.com>
2021-05-16 16:30:41 +0200

    Corrected output format

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Palette_extractor
+
+## Usage
+Simply type
+```
+cargo run /path/to/myimage.png mypalettename
+```
+
+## Todo
+1. Organize resulting palette by hue: might need to transform the rgb to hsv
+and order by h
+2. Create structs that contain the rgb and the rgb list of colors.
+3. Give the option of eliminating colors that are too similar.
+4. Create release binary of the code.
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
-use std::fmt::Write;
 use std::collections::HashSet;
+use std::env;
+use std::fmt::Write;
 use std::fs;
 
-fn rgb_to_hex(rgb : String) -> (String,String) {
-    let split = rgb.split("\t");
-    let mut hex = String::from("");
-
-    //Iterating over r, g and b
-    for col in split{
-        if col == "" {
-            continue;
-        }
-        write!(hex,"{:x}",col.parse::<u32>()
-                            .expect("Could not convert to hex"));
-    }
-
-    (rgb,hex)
-}
+//As a first argument it expects the name of the file for which you want the palette
+//As a second argument is the name you want to give to the palette
 
 fn main() {
-    let img = image::open("../test.png")
-                .expect("Could not read image")
-                .to_rgb8();
+
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let palette_name = &args[2];
+
+    let img = image::open(filename)
+    .expect("Could not read image")
+    .to_rgb8();
 
     //Using a set so we don't insert the same color twice
-    let mut colors : HashSet<String> = HashSet::new();
+    let mut colors: HashSet<String> = HashSet::new();
 
     //Gets values
     let raw = img.into_raw();
@@ -32,26 +25,46 @@ fn main() {
 
     let mut i = 0;
     for val in raw {
-        
         write!(rgb, "{}\t", val).expect("Could not convert");
-        i = i+1;
+        i = i + 1;
 
-        if i==3 {
-            colors.insert(rgb); 
+        if i == 3 {
+            colors.insert(rgb);
             rgb = String::from("");
-            i=0;
+            i = 0;
         }
     }
-    
-    let mut output = String::from("GIMP Palette\n");
 
-    for color in colors{
-        let (color,hex) = rgb_to_hex(color);
-        writeln!(output,"{}\t{}",color,hex);
+    let mut output = format!("GIMP Palette\n#Palette Name: {}\n#Colors: {}\n"
+                            ,palette_name, colors.len());
+
+    for color in colors {
+        let hex = rgb_to_hex(&color);
+        writeln!(output, "{}\t{}", color, hex).expect("Could not create line of rgb and hex");
     }
 
-    println!("{}",output);
-    fs::write("./palette.gpl",output).expect("Unable to write file");
+    println!("{}", output);
+    let palette_name = format!("{}.gpl",palette_name);
+    fs::write(palette_name, output).expect("Unable to write file");
+}
 
 
+fn rgb_to_hex(rgb: &String) -> String {
+    let split = rgb.split("\t");
+    let mut hex = String::from("");
+
+    //Iterating over r, g and b
+    for col in split {
+        if col == "" {
+            continue;
+        }
+        write!(
+            hex,
+            "{:x}",
+            col.parse::<u32>().expect("Could not convert rgb to hex.")
+        )
+        .expect("Could not write hex line.");
+    }
+
+    hex
 }
